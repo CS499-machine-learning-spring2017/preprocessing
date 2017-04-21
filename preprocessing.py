297be8269bfb729a89ff683966509e166911489b
crgant12 <christopher.gant1235@gmail.com>
2017-04-21 12:16:01 -0400

    started added comments, still need to add more

--- preprocessing.py
+++ preprocessing.py
@@ -17,20 +17,26 @@ class GroupData(object):
 
     def getEncoder(self, values):
         encoder = {}
+        # Loop through number of classifiers and 
+        # generate One Hot encoding schema
         for i, num in enumerate(values):
             data = [0 for _ in range(len(values))]
             data[i] = 1
+            # Save it in the dictionary
             encoder[int(num)] = data
         return encoder
 
     def getData(self, window):
         '''Returns a tuple with the flat data and the corresponding classifier'''
+        # get the line and classifier generators to match them up
+        # and return them to the neural network
         line = self.inputData.getLine()
         classifier = self.classData.getClassifier()
         # print(self.classData.counts)
+        # Find the minimun value of the classifier and limit all results to that 
         minClassCounter = min(self.classData.counts.values())
+        #Create the one hot encoder
         hotEncoder = self.getEncoder(sorted(self.classData.counts.keys()))
-        print(hotEncoder)
         classCounter = {}
         rowcount = 0
         shouldContinue = True
@@ -39,13 +45,16 @@ class GroupData(object):
                 newline = next(line)
                 newclassifier = next(classifier)
             except StopIteration:
+                # End of the data files
                 shouldContinue = False
             except IndexError:
+                # Error in getting the data.
+                # this has been fixed but keeping this here so if there is a bug,
+                # It doesn't crash the entire system
                 print("There was an issue with getting the correct information from the file!!!")
                 newline = None
                 newclassifier = None
                 shouldContinue = False
-
             except Exception as e:
                 print("unknown error: {}".format(e))
                 print(newline, newclassifier)
@@ -53,16 +62,19 @@ class GroupData(object):
                 newclassifier = None
                 shouldContinue = False
 
+            # No more information from files, end generating data
             if((newline is None) or (newclassifier is None)):
                 break
 
+            # See if the classifier is below the max amount
             classCounter[newclassifier] = classCounter.get(newclassifier, 0) + 1
             if(classCounter[newclassifier] > minClassCounter):
                 continue
             else:
                 rowcount += 1
+                # Return the flatten row of data and the one hot encoding classifier
                 yield (newline, hotEncoder[newclassifier])
-        print(rowcount)
+        # print(rowcount)
         return None
 
 class Data(object):
@@ -71,6 +83,8 @@ class Data(object):
     width: type int: width of each row for the csv file
     height: type int: number of rows in the csv file
     window: type int: number that describes the height and width of how much data the neural network will take in
+    middleIndex = The index to start indexing at to avoid getting the edge data since that can't be used
+    counts = counts of the classifiers
     '''
     def __init__(self, csvfile, width, height, window, counts = {}):
         self.csvfile = csvfile
@@ -86,13 +100,15 @@ class Data(object):
         '''
         if(datatype == 'line'):
             frame = []
+            # build a list of frames where each frame is a row of data
             for _ in range(self.window):
                 row = next(reader)
                 row = list(map(int, row))
                 frame.append(row)
             return frame
         else:
-            for _ in range((self.window // 2)):
+            # ignore upto the middle index amount of lines since those are edge and can't be used
+            for _ in range(self.middleIndex):
                 next(reader)
             return
 
@@ -101,37 +117,50 @@ class Data(object):
         Takes a csv file and returns a generator function
         that will return a flat list of the moving window
         '''
+        #open the csv file
         with open(self.csvfile) as infile:
             reader = csv.reader(infile)
+            # initialize the frames used to generate the data
             frames = self.initializeFrame(reader, "line")
+            # For row in csv file
             for row in reader:
                 row = list(map(int, row))
+                # loop through the data that doesn't include the edges
                 for currIndex in range(self.middleIndex, self.width - self.middleIndex):
                     minIndex = currIndex - self.middleIndex
                     maxIndex = currIndex + self.middleIndex + 1
+                    # take part of the row that is the window size so
+                    # now you have a widow x window size list of list
                     newdata = [frame[minIndex: maxIndex] for frame in frames]
-                    #flatten the moving window
+                    #flatten the list of lists
                     yield [num for data in newdata for num in data]
-                #get new frame
+                #remove top frame and replace it with a new one
                 frames = frames[1:] + [row]
-        return
-
 
     def getClassifier(self):
         '''
         Opens a csv and yields the number in the middle of the moving window
         '''
+        # open the csv file and make it a csv object
         with open(self.csvfile) as infile:
             reader = csv.reader(infile)
+            # remove the first few line in the csv to get rid of the edge data
             _ = self.initializeFrame(reader, "classifier")
+            # For row in csv file
             for row in reader:
+                # make sure everything is an int
                 row = list(map(int, row))
+                # For each valid piece of data, yield it 
                 for currIndex in range(self.middleIndex, self.width - self.middleIndex):
                     yield row[currIndex]
-        return
 
 class Counts(object):
-
+    '''
+    Controls counting the classifiers and handles how to do that
+    cleandata = decimal data in list format
+    halfwindow = half the window size rounded down
+    counter = counter object to count all the data
+    '''
     def __init__(self, data, width, window):
         self.cleandata = data
         self.width = width
@@ -139,42 +168,49 @@ class Counts(object):
         self.counter = Counter()
 
     def count(self):
+        # batch the data into row
         for pos in range(0, len(self.cleandata), self.width):
             subdata = self.cleandata[pos: pos + self.width]
+            # Get the classifiers from the data that don't include the edge data
             subdata = subdata[self.halfwindow : -1 * self.halfwindow]
             self.counter.update(subdata)
 
-    def data(self):
+    def __dict__(self):
+        # return a dictionary of the counter object
         return dict(self.counter)
 
 
 def getDemensions(openfile):
     '''Get the height and width from the binary file'''
     try:
-        dims = (openfile)
+        # First row of data in the data files are height and width
+        dims = next(openfile)
         dims = dims.strip().split(' ')
     except:
         dims = next(openfile)
         dims = dims.decode("utf-8")
         dims = dims.strip()
         dims = dims.split(' ')
+    # they can't be strings
     width, height = map(int, dims)
     return (width, height)
 
 def cleandata(binary):
     '''converts from binary to decimal'''
     #convert binary to integers
-#    data = binary.replace("0xff", "")
+    # decodes the data
     return [d for d in binary]
 
 def getFileName(file, extension = 'csv'):
-    '''creates a new file'''
+    '''creates a new file with the given extension in the folder the data is in'''
     filepath = file.split('/')
     filepath[-1] = "cleaned_" + filepath[-1] + "." + extension
     return  "/".join(filepath)
 
 def getrows(data, width):
     '''gets rows for csv file'''
+    # create batches of data given the width the data is suppose to be in
+    # This will be fed into a csv file
     for pos in range(0, len(data), width):
         yield data[pos: pos + width]
 
@@ -182,15 +218,21 @@ def saveToFile(filename, data, lineLength):
     '''creates new csv file from decoded binary data'''
     with open(filename, "w") as outfile:
         writer = csv.writer(outfile)
+        # for row in data to csv format
         for row in getrows(data, lineLength):
-            rowstr = map(str, row)
-            writer.writerow(rowstr)
+            # save the data
+            writer.writerow(row)
     return filename
 
 def countClassifiers(data, width, window):
+    '''Counts the classifiers based on the data and the window size
+        Handled in the Counts class above
+    '''
     counts = Counts(data, width, window)
+    # count everything up based on width and window size
     counts.count()
-    return counts.data()
+    # return a dictinary of the data
+    return dict(counts)
 
 def saveDemensions(file, width, height, window, cleaneddata, classifier):
     data = {'width':width, 'height':height, 'window': window}
