0c56327894d29a4b9a802b1905184b1af3d11f5d
crgant12 <christopher.gant1235@gmail.com>
2017-04-23 19:52:53 -0400

    added test file

---
+++ test_preprocessing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import unittest
+
+class Test_preprocessing(unittest.TestCase):
+    TEST_FILE_INPUT = 'test_input'
+    TEST_FILE_ALPHA = 'test_alpha'
+
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
